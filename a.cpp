ec38802be59bb130074d6f2fc3c278f4b7719a46
ngo huy <87854612+huyngo202@users.noreply.github.com>
2024-11-15 08:09:42 +0700

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>  
+#ifdef LOCAL
+#include <D:/workspace/cp/debug.hpp>
+#endif
 using namespace std;
 using ll = long long;
 using i64 = long long;
-void __print(int x) { cerr << x; } void __print(long x) { cerr << x; } void __print(long long x) { cerr << x; } void __print(unsigned x) { cerr << x; } void __print(unsigned long x) { cerr << x; } void __print(unsigned long long x) { cerr << x; } void __print(float x) { cerr << x; } void __print(double x) { cerr << x; } void __print(long double x) { cerr << x; } void __print(char x) { cerr << '\'' << x << '\''; } void __print(const char* x) { cerr << '"' << x << '"'; } void __print(const string& x) { cerr << '"' << x << '"'; } void __print(bool x) { cerr << (x ? "true" : "false"); } template<typename T, typename V> void __print(const pair<T, V>& x) { cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}'; } template<typename T> void __print(const T& x) { int f = 0; cerr << '{'; for (auto& i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}"; } void _print() { cerr << "]\n"; } template <typename T, typename... V> void _print(T t, V... v) { __print(t); if (sizeof...(v)) cerr << ", "; _print(v...); }
-#define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
 #define ln "\n"
 #define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 #define all(x) (x).begin(), (x).end()
@@ -11,7 +12,7 @@ void __print(int x) { cerr << x; } void __print(long x) { cerr << x; } void __pr
 #define ar array
 int Bit(int mask, int b) { return (mask >> b) & 1; }
 const ll base = 311, MOD = 998244353, M = 1e9 + 7, INF = 1e18;
-
+ 
 const int mxN = 1e6 + 5;
 int n;
 
